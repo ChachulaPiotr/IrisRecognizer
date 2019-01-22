7307a5b769919921dce938a9960992981008a276
Piotr <pchachula@gmail.com>
2019-01-22 17:28:54 +0100

    Some cleaning

--- bred.py
+++ bred.py
@@ -107,7 +107,6 @@ class NN:
 
         # input activations
         for i in range(self.ni - 1):
-            # self.ai[i] = sigmoid(inputs[i])
             self.ai[i] = inputs[i]
 
         # hidden activations
@@ -123,9 +122,6 @@ class NN:
             sum = 0.0
             for j in range(self.nh):
                 sum = sum + (self.ah[j] * self.wo[k][j])
-            # print(str(sum) +"\n")
-            #self.ao[k] = sigmoid(sum)
-            #self.so[k] = self.ao[k]
             self.ao[k] = sum
         if self.ao[0] > 1000:
             print(self.ao[0])
@@ -139,7 +135,6 @@ class NN:
         output_deltas = [0.0] * self.no
         for k in range(self.no):
             error = targets[k] - self.ao[k]
-            #output_deltas[k] = dsigmoid(self.so[k]) * error
             output_deltas[k] = error
 
         # calculate error terms for hidden
@@ -147,7 +142,6 @@ class NN:
         for j in range(self.nh):
             error = 0.0
             for k in range(self.no):
-                # error = error + output_deltas[k] * self.wo[j][k]
                 error = error + output_deltas[k] * self.wo[k][j]
             hidden_deltas[j] = dsigmoid(self.sh[j]) * error
 
@@ -156,7 +150,6 @@ class NN:
             for k in range(self.no):
                 change = output_deltas[k] * self.ah[j]
                 self.wo[k][j] = self.wo[k][j] + N * change
-                # print N*change, M*self.co[j][k]
 
         # update input weights
         for i in range(self.ni):
@@ -227,14 +220,7 @@ class NN:
                 inputs = p[0]
                 targets = p[1]
                 self.update(inputs)
-                #error = self.backPropagate(targets, N)
                 error = error + self.backPropagate(targets, N)
-                #error = error + 0.5 * (targets[0] - self.ao[0]) ** 2
-                #print(error, '->', targets[0], '->', self.ao[0])
-                #print('error %-.5f' % self.backPropagate(targets, N))
-                #if (k == 10):
-                    #self.backPropagate(targets, N, error / len(patterns))
-            #self.backPropagate(targets, N, error/len(patterns))
             if i % 10 == 0:
                 print('error %-.5f' % error)
 
@@ -247,14 +233,10 @@ class NN:
 
 def demo():
     arr = []
-    arr2 = []
 
-    with open('iris2.txt') as csv_file:
+    with open('iris.txt') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            # print(row[0] + " " + row[1])
-            # print(row[2] + " " + row[3])
-            # print(row[4] + "\n")
             arr.append([])  # array for one flower
             arr[-1].append([])  # array for input data
             arr[-1].append([])  # array for output data
@@ -272,38 +254,11 @@ def demo():
             else:
                 print('Something went wrong')
             arr[-1][1].append(row[4])
-
-    with open('iris.txt') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            # print(row[0] + " " + row[1])
-            # print(row[2] + " " + row[3])
-            # print(row[4] + "\n")
-            arr2.append([])  # array for one flower
-            arr2[-1].append([])  # array for input data
-            arr2[-1].append([])  # array for output data
-
-            arr2[-1][0].append(row[0])
-            arr2[-1][0].append(row[1])
-            arr2[-1][0].append(row[2])
-            arr2[-1][0].append(row[3])
-            if (row[4] == 'Iris-setosa'):
-                row[4] = 1.0
-            elif (row[4] == 'Iris-versicolor'):
-                row[4] = 2.0
-            elif (row[4] == 'Iris-virginica'):
-                row[4] = 3.0
-            else:
-                print('Something went wrong')
-            arr2[-1][1].append(row[4])
-            # setosa = makeMatrix(50, 4, 0.0)
-            # versicolor = makeMatrix(50, 4, 0.0)
-            # virginica = makeMatrix(50, 4, 0.0)
             setosa = []
             versicolor = []
             virginica = []
             k = 0
-            for r in arr2:
+            for r in arr:
                 k = k + 1
                 if k <= 50:
                     setosa.append(r)
@@ -313,17 +268,13 @@ def demo():
                     virginica.append(r)
         print(setosa)
 
-    # for i in arr:
-    #     print(i)
-
     # create a network with four input, five hidden, and one output nodes
     n = NN(4, 5, 1)
     # train it with some patterns
-    #n.test(arr2)
 
     #setting ratio between teaching and veryfing collection
     border = 95
-    #
+    #!!!!
     border = round(50*border/100)
     teach = []
     verify = []
@@ -343,18 +294,6 @@ def demo():
     n.train(teach, 1000, 0.05)
     # test it
     n.test(verify)
-    # p = ([6.2, 3.4, 5.3, 2.3], [3.0])
-    #p = ([5.9, 3.0, 5.1, 1.8], [3.0])
-    #l = ([5.2, 2.7, 2.7, 4.0], [2.0])
-    #z = ([5.7, 4.4, 4.4, 1.5], [1.0])
-    #m = ([4.6, 3.2, 3.2, 1.4], [1.0])
-    #print(p[0], '->', n.update(p[0]), '==', round(n.update(p[0])[0]), '->', p[1])
-    #p = ([6.0, 2.7, 5.1, 1.6], [2.0])
-    #print(p[0], '->', n.update(p[0]), '==', round(n.update(p[0])[0]), '->', p[1])
-    #print(l[0], '->', n.update(l[0]), '==', round(n.update(l[0])[0]), '->', l[1])
-    #print(z[0], '->', n.update(z[0]), '==', round(n.update(z[0])[0]), '->', z[1])
-    #print(m[0], '->', n.update(m[0]), '==', round(n.update(m[0])[0]), '->', m[1])
-    #print(n.wi)
 
 
 if __name__ == '__main__':
